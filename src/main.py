c05d296ddb71c48aa72ae59ef8f8b93e02678076
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-10-01 02:11:32 +0600

    Modify the main function to test the new run on folder method

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ from detector import *
 modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8.tar.gz"
 
 classFile = "coco.names"
-imagePath = "../test/cycle.jpg"
+imagePath = "../test/cats.jpg"
+folderPath = "../test/"
 videoPath = "../test/street.mp4"  # 0 for wbcam
 threshold = 0.5
 
@@ -13,5 +14,7 @@ detector = Detector()
 detector.readClasses(classFile)
 detector.downloadModel(modelURL)
 detector.loadModel()
+detector.predictImagesInFolder(folderPath, threshold)
 detector.predictImage(imagePath, threshold)
+
 detector.predictVideo(videoPath, threshold)
