0c64b6207ed93fcecf3aa58a19d517a5331ef9cc
karit7 <120469589+karit7@users.noreply.github.com>
2023-10-04 18:25:49 +0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 import argparse
 from detector import *
 
-# modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz"
-# modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d4_coco17_tpu-32.tar.gz"
+# modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/mask_rcnn_inception_resnet_v2_1024x1024_coco17_gpu-8.tar.gz"
 modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8.tar.gz"
 
 # Parse command-line arguments
